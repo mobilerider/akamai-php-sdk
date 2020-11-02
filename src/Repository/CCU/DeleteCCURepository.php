85ad41caaf207fe679528d8ba793d3d6ea6f9dce
Michel <m1ch3lp3r3z@gmail.com>
2020-11-02 14:39:04 -0500

    Add CCU purge by deletion

---
+++ src/Repository/CCU/DeleteCCURepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Akamai\Sdk\Repository\CCU;
+
+use Akamai\Sdk\Model\CCU\DeleteCCU;
+use Akamai\Sdk\Repository\AbstractAkamaiRepository;
+use Mr\Bootstrap\Model\BaseModel;
+
+class DeleteCCURepository extends AbstractAkamaiRepository
+{
+    protected $version = 'v3';
+
+    /**
+     * @return mixed
+     */
+    public function getModelClass()
+    {
+        return DeleteCCU::class;
+    }
+
+    protected function getModelUri(BaseModel $model)
+    {
+        $resource = $this->getResourcePath();
+        $uri = sprintf(
+            "%s/%s/%s",
+            $resource,
+            $model->target,
+            $model->network
+        );
+
+        if ($model->isNew()) {
+            $uri .= "/{$model->id}";
+        }
+        
+        return $uri;
+    }
+
+    public function getResourcePath()
+    {
+        $resource = $this->getResource();
+
+        return sprintf(
+            "/ccu/%s/%s",
+            $this->version,
+            $resource
+        );
+    }
+}
